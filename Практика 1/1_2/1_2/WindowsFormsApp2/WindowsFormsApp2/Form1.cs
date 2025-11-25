238fa9af7cfbec8d4e46f3d75e2e3db18e2f94cb
pasha-yunker <pashayunker3108@gmail.com>
2025-11-25 22:12:58 +0400

    Центрирование текста

--- Практика 1/1_2/1_2/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ Практика 1/1_2/1_2/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,11 +16,19 @@ namespace WindowsFormsApp2
         public Form1()
         {
             InitializeComponent();
+            Center();
+        }
+
+        private void Center()
+        {
+            textBox1.Left = (this.ClientSize.Width - textBox1.Width) / 2;
+            textBox1.Top = (this.ClientSize.Height - textBox1.Height) / 3;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             textBox1.Text = "Привет, Павел Юнкер!";
+            Center();
         }
     }
 }
