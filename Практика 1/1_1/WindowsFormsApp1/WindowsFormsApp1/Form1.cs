9af35270329cdfd5e78b181d0361fb9936ea0c04
pasha-yunker <pashayunker3108@gmail.com>
2025-11-25 16:03:40 +0400

    Центрирование отображения лейбла

--- Практика 1/1_1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ Практика 1/1_1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -15,11 +15,19 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            Center();
+        }
+
+        private void Center()
+        {
+            label1.Left = (this.ClientSize.Width - label1.Width) / 2;
+            label1.Top = (this.ClientSize.Height - label1.Height) / 3;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             label1.Text = "Привет, Павел Юнкер!";
+            Center();
         }
     }
 }
