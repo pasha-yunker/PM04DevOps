0670b2b0d63be4e841bbc08939b267404ca4eee4
pasha-yunker <pashayunker3108@gmail.com>
2025-11-28 10:54:58 +0400

    Добавлен текст Увеличение текста по кнопке | работа Павла Юнкер

--- Практика 1/2_dop/Form1.Designer.cs
+++ Практика 1/2_dop/Form1.Designer.cs
@@ -60,7 +60,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Увеличение текста по кнопке | работа Павла Юнкер";
             this.ResumeLayout(false);
             this.PerformLayout();
 
