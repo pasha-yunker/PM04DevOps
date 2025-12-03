623e8b9268631ce3fd8620baa5941d6aeb11561b
pasha-yunker <pashayunker3108@gmail.com>
2025-12-03 13:12:09 +0400

    Загружена Практика 1

--- Практика 2/2_7.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import random
-
-num = [random.randint(0, 100) for _ in range(1000)]
-
-plt.scatter(range(1000), num)
-plt.gcf().canvas.manager.set_window_title('Точечный график 1000 случайных чисел | Работа Павла Юнкер')
-plt.show()
